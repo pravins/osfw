68cf15c864af2e9df2599e0a4f672bd852e56d59
pravins <psatpute@redhat.com>
2013-10-10 16:30:13 +0530

    updating app

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -154,7 +154,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'osfw',
+    'openshift.osfw',
 )
 
 # A sample logging configuration. The only tangible logging
