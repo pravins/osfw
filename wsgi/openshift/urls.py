083de2b138342913b5356c870512a9b38e411334
pravins <psatpute@redhat.com>
2013-10-10 12:05:21 +0530

    Revert "added file from locale project"
    This reverts commit bce9b3a540938f6bf9faa70b7d47120e2286f9b1.

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -6,23 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'osfw.views.redirect_to_index'),
-    url(r'^index/$', 'osfw.views.index'),
-    url(r'^about/$', 'osfw.views.about'),
-    url(r'^peoples/$', 'osfw.views.peoples'),
-    url(r'^standards/$', 'osfw.views.standards'),
-    url(r'^index/allfonts/$', 'osfw.views.allfonts'),
-    url(r'^index/fontinfo/$', 'osfw.views.fontinfo'),
-#    url(r'^index/searchfonts/$', 'osfw.views.search_fonts'),
-    url(r'^index/search/$', 'osfw.views.search'),
-    url(r'^index/test/$', 'osfw.views.test'),
-
-    url(r'^get/(?P<osfw_id>\d+)/', 'osfw.views.fontinfo'),
-    url(r'^like/(?P<osfw_id>\d+)/', 'osfw.views.likefont'),
-
-#    url(r'^osfw/$', include('osfw.urls')),
-    # url(r'^$', 'myprj.views.home', name='home'),
-    # url(r'^myprj/', include('myprj.foo.urls')),
+    url(r'^$', 'openshift.views.home', name='home'),
+    # url(r'^openshift/', include('openshift.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
