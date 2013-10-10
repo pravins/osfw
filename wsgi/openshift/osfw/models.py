899116d23665f2470ddf6524419f6233a6f07d7c
pravins <psatpute@redhat.com>
2013-10-10 16:48:33 +0530

    updated model

--- wsgi/openshift/osfw/models.py
+++ wsgi/openshift/osfw/models.py
@@ -4,13 +4,15 @@ from django.db import models
 class Osfw(models.Model):
 	familyname = models.CharField(max_length=35)
 	subfamily = models.CharField(max_length=35)
-	version = models.CharField(max_length=100)
+	version = models.CharField(max_length=50)
 	license = models.CharField(max_length=1000)
-	upstreamurl = models.CharField(max_length=300)
+	upstreamurl = models.CharField(max_length=100)
 	langsupport = models.CharField(max_length=1000)
-	scriptsupport = models.CharField(max_length=1000)
-	ttffilename = models.CharField(max_length=100)
+	scriptsupport = models.CharField(max_length=200)
+	ttffilename = models.CharField(max_length=50)
+	fontformat = models.CharField(max_length=30)
 	copyright = models.CharField(max_length=1000)
+	imagename = models.CharField(max_length=50)
 	likes = models.IntegerField(default=0)
 	
 	def __unicode__(self):
