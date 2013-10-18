971188711247013098bcaef43cec38168140afdd
pravins <psatpute@redhat.com>
2013-10-18 17:10:58 +0530

    added captch field

--- wsgi/openshift/registration/forms.py
+++ wsgi/openshift/registration/forms.py
@@ -12,6 +12,7 @@ you're using a custom model.
 from django.contrib.auth.models import User
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from captcha.fields import CaptchaField
 
 
 class RegistrationForm(forms.Form):
@@ -38,6 +39,8 @@ class RegistrationForm(forms.Form):
                                 label=_("Password"))
     password2 = forms.CharField(widget=forms.PasswordInput,
                                 label=_("Password (again)"))
+    captcha = CaptchaField()
+
     
     def clean_username(self):
         """
