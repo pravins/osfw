331c221f54efb267a02f683f8bf88c5c2470e072
pravins <psatpute@redhat.com>
2013-11-01 23:46:27 +0530

    added redirect

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,5 +1,5 @@
 import os
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
