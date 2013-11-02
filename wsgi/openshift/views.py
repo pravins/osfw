01df93cc97c398347dca98e337e49be6844a62a6
pravins <psatpute@redhat.com>
2013-11-02 09:49:06 +0530

    updated other templates and functions for openid

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -11,7 +11,7 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from social.backends.google import GooglePlusAuth
 
-
+# Functions associated with openid
 def home(request):
     """Home view, displays login mechanism"""
     if request.user.is_authenticated():
@@ -19,6 +19,34 @@ def home(request):
     return render_to_response('home.html', {
         'plus_id': getattr(settings, 'SOCIAL_AUTH_GOOGLE_PLUS_KEY', None)
     }, RequestContext(request))
+
+def require_email(request):
+    if request.method == 'POST':
+        request.session['saved_email'] = request.POST.get('email')
+        backend = request.session['partial_pipeline']['backend']
+        return redirect('social:complete', backend=backend)
+    return render_to_response('email.html', RequestContext(request))
+
+@login_required
+def done(request):
+    """Login complete view, displays user data"""
+    scope = ' '.join(GooglePlusAuth.DEFAULT_SCOPE)
+    return render_to_response('done.html', {
+        'user': request.user,
+        'plus_id': getattr(settings, 'SOCIAL_AUTH_GOOGLE_PLUS_KEY', None),
+        'plus_scope': scope
+    }, RequestContext(request))
+
+def validation_sent(request):
+    return render_to_response('validation_sent.html', {
+        'email': request.session.get('email_validation_address')
+    }, RequestContext(request))
+
+def signup_email(request):
+    return render_to_response('email_signup.html', {}, RequestContext(request))
+
+
+
 
 
 def login(request):
